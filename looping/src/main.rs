dfae11911482dbf9b4d4b43dc3d3a47ffd0c34ad
dgaye <damegaye1887@gmail.com>
2024-04-22 13:28:11 +0000

    looping

---
+++ looping/src/main.rs
@@ -0,0 +1,22 @@
+use std::io;
+
+fn main() {
+    let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place.";
+    let answer = "the letter e";
+    let mut tries = 0;
+
+    loop {
+        println!("{}", riddle);
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).expect("Failed to read line");
+
+        if user_input.trim() == answer {
+            tries += 1;
+            println!("Number of trials: {}", tries);
+            break;
+        } else {
+            tries += 1;
+        }
+    }
+}
+

749be5145d68f0e44563350bda0692b975ec8341
mabdelwe <mouhamedvadel2021@gmail.com>
2024-04-23 14:36:30 +0000

    fixing repis too large problem

--- looping/target/debug/deps/looping-4da57f279f8d28f6.d
+++
@@ -1,5 +0,0 @@
-/home/fadel/Desktop/piscine-rust/looping/target/debug/deps/liblooping-4da57f279f8d28f6.rmeta: src/main.rs
-
-/home/fadel/Desktop/piscine-rust/looping/target/debug/deps/looping-4da57f279f8d28f6.d: src/main.rs
-
-src/main.rs:

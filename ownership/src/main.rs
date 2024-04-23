1a2d00891834d9622d250e2b815b6539d20a201a
mabdelwe <mouhamedvadel2021@gmail.com>
2024-04-23 19:09:34 +0000

    names initilas

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,14 @@
+fn main() {
+	let s1 = String::from("helloWorld");
+	let s2 = String::from("snake_case");
+	let s3 = String::from("CamelCase");
+	let s4 = String::from("just");
+
+	println!("first_subword({}) = {}", s1.clone(), first_subword(s1));
+	println!("first_subword({}) = {}", s2.clone(), first_subword(s2));
+	println!("first_subword({}) = {}", s3.clone(), first_subword(s3));
+	println!("first_subword({}) = {}", s4.clone(), first_subword(s4));
+}
 
 pub fn first_subword(mut s: String) -> String {
     let mut subword = String::new();
@@ -19,3 +30,4 @@ pub fn first_subword(mut s: String) -> String {
 
     subword
 }
+

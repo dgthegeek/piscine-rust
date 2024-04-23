f76d82d754ed360d2f5ea71bd693d36b13f65c5c
mabdelwe <mouhamedvadel2021@gmail.com>
2024-04-23 14:56:11 +0000

    tuple..

---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)]
+pub struct Student(pub u32, pub String, pub String);
+
+pub fn id(student: &Student) -> u32 {
+    student.0
+}
+
+pub fn first_name(student: &Student) -> String {
+    student.1.clone()
+}
+
+pub fn last_name(student: &Student) -> String {
+    student.2.clone()
+}

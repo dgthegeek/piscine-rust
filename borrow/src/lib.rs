1c278b08bc99bc4ea79f50f9eb48a393144540c3
mabdelwe <mouhamedvadel2021@gmail.com>
2024-04-23 16:46:49 +0000

    borrowing

---
+++ borrow/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn str_len(s: impl AsRef<str>) -> usize {
+    s.as_ref().len()
+}
---
+++ looping/target/debug/deps/looping-ef436ed9274b8e5a.d
@@ -0,0 +1,5 @@
+/home/fadel/Desktop/piscine-rust/looping/target/debug/deps/liblooping-ef436ed9274b8e5a.rmeta: src/main.rs
+
+/home/fadel/Desktop/piscine-rust/looping/target/debug/deps/looping-ef436ed9274b8e5a.d: src/main.rs
+
+src/main.rs:

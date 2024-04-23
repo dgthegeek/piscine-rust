2936cd294fdf818513fb30712fab03e3f036293b
mabdelwe <mouhamedvadel2021@gmail.com>
2024-04-23 15:02:47 +0000

    to large

--- reverse_string/target/debug/deps/reverse_string-c663d43e16c9283e.d
+++
@@ -1,5 +0,0 @@
-/home/fadel/Desktop/piscine-rust/reverse_string/target/debug/deps/libreverse_string-c663d43e16c9283e.rmeta: src/lib.rs
-
-/home/fadel/Desktop/piscine-rust/reverse_string/target/debug/deps/reverse_string-c663d43e16c9283e.d: src/lib.rs
-
-src/lib.rs:
--- speed_transformation/target/debug/deps/speed_transformation-0601528164052851.d
+++
@@ -1,5 +0,0 @@
-/home/fadel/Desktop/piscine-rust/speed_transformation/target/debug/deps/libspeed_transformation-0601528164052851.rmeta: src/lib.rs
-
-/home/fadel/Desktop/piscine-rust/speed_transformation/target/debug/deps/speed_transformation-0601528164052851.d: src/lib.rs
-
-src/lib.rs:

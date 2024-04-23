4a06be662f49db8a36d21f8e12cd5097db9c7b4e
mabdelwe <mouhamedvadel2021@gmail.com>
2024-04-23 18:52:22 +0000

    names initilas

--- fibonacci2/target/debug/deps/fibonacci2-72c247ef8f28b6d4.d
+++
@@ -1,5 +0,0 @@
-/home/fadel/Desktop/piscine-rust/fibonacci2/target/debug/deps/libfibonacci2-72c247ef8f28b6d4.rmeta: src/lib.rs
-
-/home/fadel/Desktop/piscine-rust/fibonacci2/target/debug/deps/fibonacci2-72c247ef8f28b6d4.d: src/lib.rs
-
-src/lib.rs:

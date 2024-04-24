b42ed306e6027e0194dad6515fcd9f3567b06cd8
dgaye <damegaye1887@gmail.com>
2024-04-24 23:57:57 +0000

    borrow me the reference

--- copy/target/debug/deps/copy-c7192dc53381f687.d
+++
@@ -1,5 +0,0 @@
-/home/student/Desktop/piscine-rust/copy/target/debug/deps/libcopy-c7192dc53381f687.rmeta: src/lib.rs
-
-/home/student/Desktop/piscine-rust/copy/target/debug/deps/copy-c7192dc53381f687.d: src/lib.rs
-
-src/lib.rs:

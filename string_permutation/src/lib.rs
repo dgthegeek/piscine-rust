67a588743000fc28788dbbeb3f456fc2c401ea5c
dgaye <damegaye1887@gmail.com>
2024-04-26 19:23:15 +0000

    s

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -1,4 +1,15 @@
 
+fn main() {
+	let word = "codde";
+	let word1 = "deeco";
+	println!(
+		"Is `{}` a permutation of `{}`? = {}",
+		word,
+		word1,
+		is_permutation(word, word1)
+	);
+}
+
 
 pub fn is_permutation(s1: &str, s2: &str) -> bool {
    if s1.len() != s2.len(){
@@ -7,8 +18,13 @@ pub fn is_permutation(s1: &str, s2: &str) -> bool {
     for c in s1.chars(){
         if !s2.contains(c){
             return false
+        }else{
+            if s1.chars().filter(|&e| e == c).count() != s2.chars().filter(|&e| e == c).count(){
+                return false 
+            }
         }
     }
 
     true
 }
+

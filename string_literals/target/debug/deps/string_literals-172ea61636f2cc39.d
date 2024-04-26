4962c0d815700f1990cb5ee30d3d7bcc1b12a85f
dgaye <damegaye1887@gmail.com>
2024-04-26 18:44:16 +0000

    f

--- string_literals/target/debug/deps/string_literals-172ea61636f2cc39.d
+++
@@ -1,5 +0,0 @@
-/home/student/Desktop/piscine-rust/string_literals/target/debug/deps/libstring_literals-172ea61636f2cc39.rmeta: src/lib.rs
-
-/home/student/Desktop/piscine-rust/string_literals/target/debug/deps/string_literals-172ea61636f2cc39.d: src/lib.rs
-
-src/lib.rs:

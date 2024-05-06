fc6a6c0b7a937b37a90eafc2af2fda60814d3758
dgaye <damegaye1887@gmail.com>
2024-05-06 15:20:54 +0000

    box it baby

---
+++ box_it/src/main.rs
@@ -0,0 +1,37 @@
+pub fn transform_and_save_on_heap(s: String) -> Box<Vec<u32>> {
+    let mut numbers: Vec<u32> = Vec::new();
+
+    for token in s.split_whitespace() {
+        if token.ends_with('k') {
+            
+            let mut value = token[0..token.len()-1].parse::<f64>().unwrap();
+            value *= 1000.0;
+            
+            numbers.push(value as u32);
+        }else{
+            let value = token.parse::<u32>().unwrap_or(0);
+            numbers.push(value);
+        }
+
+
+    }
+    Box::new(numbers)
+}
+
+pub fn take_value_ownership(a: Box<Vec<u32>>) -> Vec<u32> {
+    *a
+}
+
+fn main() {
+    let new_str = String::from("5.5k 8.9k 32");
+
+    // creating a variable and we save it in the Heap
+    let a_h = transform_and_save_on_heap(new_str);
+    println!("Box value : {:?}", &a_h);
+    println!("size occupied in the stack : {:?} bytes", (std::mem::size_of_val(&a_h)));
+
+    let a_b_v = take_value_ownership(a_h);
+    println!("value : {:?}", &a_b_v);
+    println!("size occupied in the stack : {:?} bytes", (std::mem::size_of_val(&a_b_v)));
+    // whenever the box, in this case "a_h", goes out of scope it will be deallocated, freed
+}

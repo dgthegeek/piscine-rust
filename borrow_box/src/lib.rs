c8562ce9884f57e0a18d2b43ec8f2674de17e89c
dgaye <damegaye1887@gmail.com>
2024-05-08 01:25:22 +0000

    borrow_box

---
+++ borrow_box/src/lib.rs
@@ -0,0 +1,44 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct GameSession {
+    pub id: u32,
+    pub p1: (String, u16),
+    pub p2: (String, u16),
+    pub nb_games: u16
+}
+
+impl GameSession {
+    pub fn new(id: u32, p1_name: String, p2_name: String, nb_games: u16) -> Box<GameSession> {
+		let res = GameSession {
+			id: id,
+			p1: (p1_name, 0),
+			p2: (p2_name, 0),
+			nb_games: nb_games
+		};
+		Box::new(res)
+    }
+    pub fn read_winner(&self) -> (String, u16) {
+		let p1_score = self.p1.1;
+		let p2_score = self.p2.1;
+		if p1_score > p2_score {
+			self.p1.clone()
+		} else if p1_score < p2_score {
+			self.p2.clone()
+		} else {
+			("Same score! tied".to_string(), p1_score)
+		}
+    }
+    pub fn update_score(&mut self, user_name: String) {
+		if self.p1.1 + self.p2.1 == self.nb_games || 
+        (self.p1.0 != user_name && self.p2.0 != user_name) ||
+        self.p1.1 > self.nb_games / 2 || self.p2.1 > self.nb_games / 2 {
+            return;
+        }else if self.p1.0 == user_name {
+            self.p1.1 += 1;
+        }else if self.p2.0 == user_name {
+            self.p2.1 += 1;
+        }
+    }
+    pub fn delete(self) -> String {
+		format!("game deleted: id -> {}", self.id)
+    }
+}
--- box_it/src/lib.rs
+++ box_it/src/lib.rs
@@ -21,17 +21,3 @@ pub fn transform_and_save_on_heap(s: String) -> Box<Vec<u32>> {
 pub fn take_value_ownership(a: Box<Vec<u32>>) -> Vec<u32> {
     *a
 }
-
-fn main() {
-    let new_str = String::from("5.5k 8.9k 32");
-
-    // creating a variable and we save it in the Heap
-    let a_h = transform_and_save_on_heap(new_str);
-    println!("Box value : {:?}", &a_h);
-    println!("size occupied in the stack : {:?} bytes", (std::mem::size_of_val(&a_h)));
-
-    let a_b_v = take_value_ownership(a_h);
-    println!("value : {:?}", &a_b_v);
-    println!("size occupied in the stack : {:?} bytes", (std::mem::size_of_val(&a_b_v)));
-    // whenever the box, in this case "a_h", goes out of scope it will be deallocated, freed
-}
